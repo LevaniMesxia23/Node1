0f00f256e4c23ccfe437a0ed835ef2ed99afb1db
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-07-31 18:30:19 +0400

    add modules and test-module

--- 2-how-node-works/starter/streams.js
+++ 2-how-node-works/starter/streams.js
@@ -8,18 +8,22 @@ server.on('request', (req, res) => {
   //     res.end(data)
   // })
   // Solution 2
-  const readable = fs.createReadStream('test-file.txt')
-  readable.on('data', chunk => {
-    res.write(chunk)
-  })
-  readable.on('end', () => {
-    res.end()
-  })
-  readable.on('error', err => {
-    console.log(err)
-    res.statusCode = 500
-    res.end("File not found...")
-  })
+  // const readable = fs.createReadStream('test-file.txt')
+  // readable.on('data', chunk => {
+  //   res.write(chunk)
+  // })
+  // readable.on('end', () => {
+  //   res.end()
+  // })
+  // readable.on('error', err => {
+  //   console.log(err)
+  //   res.statusCode = 500
+  //   res.end("File not found...")
+  // })
+
+  // Solution 3
+  const readable = fs.createReadStream('testt-file.txt')
+  readable.pipe(res)
 })
 
 server.listen(8000, '127.0.0.1', () => {
