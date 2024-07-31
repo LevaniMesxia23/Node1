ee6f7007d9d3f732f8b283fefe9b15bfbe30720f
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-07-31 18:38:42 +0400

    add Module.exports and exports

--- 2-how-node-works/starter/modules.js
+++ 2-how-node-works/starter/modules.js
@@ -1,6 +1,14 @@
 // console.log(arguments)
 // console.log(require('module').wrapper)
 
+//Module.exports
 const C = require('./test-module-1')
 const calc1 = new C()
 console.log(calc1.add(2,5))
+
+// exports
+// const calc2 = require('./test-module-2')
+// console.log(multiply(3,5))
+
+const { add, multiply, divide } = require('./test-module-2')
+console.log(add(3,5))
