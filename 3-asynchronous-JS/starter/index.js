badf6e677f1b91f0851fcd726fef4643717285f6
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-08-01 15:43:45 +0400

    add asynchronous with promises

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -4,11 +4,15 @@ const superagent = require('superagent');
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
 
-  superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
+  superagent
+  .get(`https://dog.ceo/api/breed/${data}/images/random`)
+  .then(res => {
     console.log(res.body.message);
     if(err) return console.log(err.message)
-    fs.writeFile('dog-img.txt', res.body.message, (err) => {
+    fs.writeFile('dog-img.txt', res.body.message, err => {
       console.log('Random dog image saved to file!');
     });
-  });
+  }).catch(err => {
+    console.log(err.message)
+  })
 });
