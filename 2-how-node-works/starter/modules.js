33fa8844d6c29dfa86fe3339d25c40d3d18fe0f6
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-08-01 15:15:55 +0400

    add prettier

--- 2-how-node-works/starter/modules.js
+++ 2-how-node-works/starter/modules.js
@@ -2,13 +2,18 @@
 // console.log(require('module').wrapper)
 
 //Module.exports
-const C = require('./test-module-1')
-const calc1 = new C()
-console.log(calc1.add(2,5))
+const C = require('./test-module-1');
+const calc1 = new C();
+console.log(calc1.add(2, 5));
 
 // exports
 // const calc2 = require('./test-module-2')
 // console.log(multiply(3,5))
 
-const { add, multiply, divide } = require('./test-module-2')
-console.log(add(3,5))
+const { add, multiply, divide } = require('./test-module-2');
+console.log(add(3, 5));
+
+//caching
+require('./test-module-3')();
+require('./test-module-3')();
+require('./test-module-3')();
