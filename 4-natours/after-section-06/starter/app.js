c503d8c1ddcbd8ae5bddc234e17e0aa2567e4edc
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-08-03 19:31:49 +0400

    handling Get requests

--- 4-natours/after-section-06/starter/app.js
+++ 4-natours/after-section-06/starter/app.js
@@ -1,13 +1,23 @@
+const fs = require('fs')
+const path = require('path');
 const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.status(200).json({message: "Hello from the server side!", app: "Natours"})
-})
+// app.get('/', (req, res) => {
+//   res.status(200).json({message: "Hello from the server side!", app: "Natours"})
+// })
 
-app.post('/', (req, res) => {
-  res.send("You can post on this endpoint...")
+// app.post('/', (req, res) => {
+//   res.send("You can post on this endpoint...")
+// })
+
+const filePath = path.join(__dirname, '..', 'dev-data', 'data', 'tours-simple.json');
+const data = fs.readFileSync(filePath, 'utf-8');
+const tours = JSON.parse(data);
+
+app.get('/api/v1/tours', (req,res) => {
+  res.status(200).json({status: "success",results: tours.length, data : {tours}})
 })
 
 const port = 3000;
