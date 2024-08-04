3d8cd56c2d6fcca50288eac2310d07f2a950e330
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-08-04 14:17:56 +0400

    Save changes before reverting

--- 4-natours/after-section-06/starter/app.js
+++ 4-natours/after-section-06/starter/app.js
@@ -1,14 +1,45 @@
+const fs = require('fs')
+const path = require('path');
 const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.status(200).json({message: "Hello from the server side!", app: "Natours"})
+app.use(express.json())
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({message: "Hello from the server side!", app: "Natours"})
+// })
+
+// app.post('/', (req, res) => {
+//   res.send("You can post on this endpoint...")
+// })
+
+const filePath = path.join(__dirname, '..', 'dev-data', 'data', 'tours-simple.json');
+const data = fs.readFileSync(filePath, 'utf-8');
+const tours = JSON.parse(data);
+
+app.get('/api/v1/tours', (req,res) => {
+  res.status(200).json({status: "success",results: tours.length, data : {tours}})
 })
 
-app.post('/', (req, res) => {
-  res.send("You can post on this endpoint...")
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body)
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({id: newId,}, req.body)
+
+  tours.push(newTour)
+
+  fs.writeFile(filePath, JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour
+      }
+    })
+  })
+
 })
+
 
 const port = 3000;
 app.listen(port, () => {
