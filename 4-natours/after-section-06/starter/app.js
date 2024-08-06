c26154b528196ef5a7c322e93bc16154fc0c2067
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-08-06 19:59:13 +0400

    responding to URL parameters

--- 4-natours/after-section-06/starter/app.js
+++ 4-natours/after-section-06/starter/app.js
@@ -30,8 +30,25 @@ app.get('/api/v1/tours', (req, res) => {
     .json({ status: 'success', results: tours.length, data: { tours } });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
+  // if(id > tours.length){ or -->
+    if(!tour){
+    return res.status(404).json({
+      status: "fail",
+      message: "invalid id"
+  })
+  }
+  console.log(req.params);
+  res.status(200).json({ status: 'success',
+     data: { 
+       tour 
+    } 
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
-  // console.log(req.body)
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
