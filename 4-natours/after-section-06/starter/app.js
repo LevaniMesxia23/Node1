b53cf43e41dc1f41189455fe54991462df21909e
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-08-06 20:35:08 +0400

    Refactoring routes

--- 4-natours/after-section-06/starter/app.js
+++ 4-natours/after-section-06/starter/app.js
@@ -93,12 +93,14 @@ const createTour = (req, res) => {
   });
 }
 
-app.get('/api/v1/tours', getAllTours);
-app.get('/api/v1/tours/:id', getTour);
-app.post('/api/v1/tours', createTour);
-app.patch('/api/v1/tours/:id', updateTour)
-app.delete('/api/v1/tours/:id', );
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour)
+// app.delete('/api/v1/tours/:id', deleteTour);
 
+app.route('/api/v1/tours').get(getAllTours).post(createTour)
+app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour)
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
