fdbefe1801e35fc559fd75c3fc5d1e451c755708
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-08-17 20:23:38 +0400

    install morgan

--- 4-natours/after-section-06/starter/app.js
+++ 4-natours/after-section-06/starter/app.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const morgan = require('morgan')
 
 const app = express();
 
+// 1) Middlewares
+app.use(morgan('dev'))
 app.use(express.json());
 
 app.use((req, res, next) => {
